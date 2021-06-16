fd9380df1d1327f0e1e0a9b47db383bb2328ae25
ab55al <abdullah5590x@gmail.com>
2021-06-16 22:04:48 +0300

    Changed window manager

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -595,12 +595,14 @@ bling.module.flash_focus.enable()
 
 
 awful.spawn.with_shell("picom")
-awful.spawn.with_shell("redshift -O 3750")
+-- awful.spawn.with_shell("redshift -O 3750")
 
 awful.spawn.with_shell("setxkbmap -layout \"us(colemak),ar,us,\" -option \"grp:alt_shift_toggle\" -option caps:swapescape");
+
+
+awful.spawn.with_shell("feh --bg-fill --randomize ~/Pictures/Wallpapers/*")
 -- awful.spawn.with_shell("xinput -set-prop \"Cooler Master Technology Inc. MM710 Gaming Mouse\" 150 0.4 0 0 0 0.4 0 0 0 0.9")
 
 -- awful.spawn.with_shell("xset s off && xset s noblank && xset -dpms")
 
-awful.spawn.with_shell("nitrogen --restore")
 --local autostart = require("autostart")

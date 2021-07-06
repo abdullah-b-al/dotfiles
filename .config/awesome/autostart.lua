425e4983fc63dd52b5ec885fc873f38101ae4c13
ab55al <abdullah5590x@gmail.com>
2021-07-06 15:16:36 +0300

    Removed escape and capslock swapping

--- .config/awesome/autostart.lua
+++ .config/awesome/autostart.lua
@@ -3,7 +3,7 @@ local awful = require("awful")
 
 awful.spawn.with_shell("picom")
 awful.spawn.with_shell("redshift -O 3750")
-awful.spawn.with_shell("setxkbmap -layout 'us(colemak),ar,us,' -option 'grp:alt_shift_toggle' -option caps:swapescape");
+awful.spawn.with_shell("setxkbmap -layout 'us(colemak),ar,us,' -option 'grp:alt_shift_toggle'");
 awful.spawn.with_shell("feh --bg-fill --randomize ~/Pictures/Wallpapers/*")
 
 awful.spawn.with_shell("xinput -set-prop 'Cooler Master Technology Inc. MM710 Gaming Mouse' 'Coordinate Transformation Matrix' 0.4 0 0 0 0.4 0 0 0 0.9")

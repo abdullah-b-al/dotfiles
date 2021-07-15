ff6a2ae79dacf6609542bcdf5fb47dee96d7a191
ab55al <abdullah5590x@gmail.com>
2021-07-15 06:27:58 +0300

    Added lua lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,7 +17,7 @@ opt.number         = true
 opt.relativenumber = true
 opt.spell          = true
 opt.dictionary     = opt.dictionary + '/usr/share/dict/words'
-opt.spellfile      = '~/.config/vim/spell/en.utf-8.add' 
+opt.spellfile      = '~/.config/vim/spell/en.utf-8.add'
 opt.updatetime     = 1000
 opt.timeoutlen     = 300
 
@@ -39,9 +39,9 @@ local paq = require('paq-nvim').paq
 paq {'savq/paq-nvim', opt = true}       -- paq-nvim manages itself
 paq {'romainl/vim-cool'}                -- disables search highlighting when you are done searching and re-enables it when you search again. 
 paq {'vim-scripts/c.vim'}               -- C IDE
-paq {'ap/vim-css-color'} 
+paq {'ap/vim-css-color'}
 paq {'vimwiki/vimwiki'}
-paq {'907th/vim-auto-save'}  
+paq {'907th/vim-auto-save'}
 paq {'scrooloose/syntastic'}
 paq {'tpope/vim-surround'}
 paq {'joom/vim-commentary'}
@@ -51,6 +51,10 @@ paq {'jiangmiao/auto-pairs'}
 paq {'itchyny/lightline.vim'}
 paq {'easymotion/vim-easymotion'}
 paq {'tpope/vim-repeat'}
+paq {'neovim/nvim-lspconfig'}
 
 -- Mappings
 require('keys/keymappings')
+
+-- Lsp servers
+require('lsps/lua_server')

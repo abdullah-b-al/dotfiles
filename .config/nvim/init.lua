5df54fc953eb21751450e82be02aadb441bcb665
ab55al <abdullah5590x@gmail.com>
2021-07-21 02:27:58 +0300

    Organized and added comments.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,29 +6,31 @@ local api = vim.api
 
 
 -- Options
-opt.tabstop        = 4
-opt.softtabstop    = 4
-opt.shiftwidth     = 4
-opt.expandtab      = true
-opt.smartindent    = true
-opt.showmode       = false
-opt.nu             = true
-opt.wrap           = false
-opt.smartcase      = true
-opt.incsearch      = true
-opt.number         = true
-opt.relativenumber = true
-opt.spell          = true
-opt.dictionary     = opt.dictionary + '/usr/share/dict/words'
-opt.spellfile      = '~/.config/vim/spell/en.utf-8.add'
+opt.tabstop        = 4                                              -- Tab width in spaces
+opt.softtabstop    = 4                                              -- Tab width in spaces when performing editing operations
+opt.shiftwidth     = 4                                              -- Number of spaces to use for each step of (auto)indent
 opt.updatetime     = 1000
 opt.timeoutlen     = 300
-opt.viewoptions    = 'cursor,folds'                                -- save/restore just these with {mk,load}view`
+
+opt.expandtab      = true
+opt.smartindent    = true
+opt.number         = true                                           -- Current line number
+opt.smartcase      = true
+opt.incsearch      = true                                           -- Highlights search results as you type
+opt.relativenumber = true
+opt.spell          = true
+
+opt.showmode       = false
+opt.wrap           = false
+
+opt.dictionary     = opt.dictionary + '/usr/share/dict/words'
+opt.spellfile      = '~/.config/vim/spell/en.utf-8.add'
+opt.viewoptions    = 'cursor,folds'                                 -- save/restore just these with {mk,load}view`
 
 
 -- Commands
 cmd 'packadd paq-nvim'
-cmd 'colorscheme focuspoint'
+cmd 'colorscheme gotham256'
 cmd ([[
 augroup AutoSaveFolds
     autocmd!
@@ -51,15 +53,15 @@ g.maplocalleader = '\\'
 
 -- Plugins
 local paq = require('paq-nvim').paq
-    paq {'savq/paq-nvim', opt = true}       -- paq-nvim manages itself
-    paq {'romainl/vim-cool'}                -- disables search highlighting when you are done searching and re-enables it when you search again. 
-    paq {'vim-scripts/c.vim'}               -- C IDE
+    paq {'savq/paq-nvim', opt = true}                               -- paq-nvim manages itself
+    paq {'romainl/vim-cool'}                                        -- disables search highlighting when you are done searching and re-enables it when you search again. 
+    paq {'vim-scripts/c.vim'}                                       -- C IDE
+    paq {'christoomey/vim-system-copy'}                             -- Requires xsel
     paq {'ap/vim-css-color'}
     paq {'vimwiki/vimwiki'}
     paq {'scrooloose/syntastic'}
     paq {'tpope/vim-surround'}
     paq {'joom/vim-commentary'}
-    paq {'christoomey/vim-system-copy'}
     paq {'michaeljsmith/vim-indent-object'}
     paq {'jiangmiao/auto-pairs'}
     paq {'itchyny/lightline.vim'}

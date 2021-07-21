2d8893760e9fc6fe35f7b1c092c7db1506e22181
ab55al <abdullah5590x@gmail.com>
2021-07-21 06:37:17 +0300

    Changed mappings of Telescope

--- .config/nvim/lua/keys/keymappings.lua
+++ .config/nvim/lua/keys/keymappings.lua
@@ -40,10 +40,10 @@ end
 
 
 -- Telescope
-   map('n', '<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<CR>', nore)
-   map('n', '<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<CR>', nore)
-   map('n', '<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<CR>', nore)
-   map('n', '<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<CR>', nore)
+   map('n', '<leader>tf', '<cmd>lua require("telescope.builtin").find_files()<CR>', nore)
+   map('n', '<leader>tg', '<cmd>lua require("telescope.builtin").live_grep()<CR>', nore)
+   map('n', '<leader>tb', '<cmd>lua require("telescope.builtin").buffers()<CR>', nore)
+   map('n', '<leader>th', '<cmd>lua require("telescope.builtin").help_tags()<CR>', nore)
 
 
 -- easy motion mappings

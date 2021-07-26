a0e63d256a914391e301a41aeac0f66586757fb1
ab55al <abdullah5590x@gmail.com>
2021-07-26 12:48:36 +0300

    Nvim: Completion-nvim noinsert added

--- .config/nvim/plugin/completion-nvim.lua
+++ .config/nvim/plugin/completion-nvim.lua
@@ -1,8 +1,5 @@
-vim.api.nvim_exec(
-[[
+vim.cmd([[
 inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
 inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
-]]
-,false
-)
-vim.opt.completeopt ={"menuone", "noinsert"}
+]])
+vim.opt.completeopt ={"menuone", "noinsert", "noselect"}

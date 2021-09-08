18e5e5eb0d31b3f63a054cb86220d42d7cccf433
ab55al <abdullah5590x@gmail.com>
2021-09-08 23:38:13 +0300

    Awesome: Added window rules for games

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -483,6 +483,13 @@ awful.rules.rules = {
         }
     }, properties = { opacity = true } },
 
+    -- Rules for games
+    { rule_any = {
+        class = {
+            "steam_app"
+        }
+    }, properties = { floating = true, screen = 1, tag = "5", fullscreen = true}}
+
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },

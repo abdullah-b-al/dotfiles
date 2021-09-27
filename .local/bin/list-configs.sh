613b3f248e2cbcacb91f594bf0eb34e8ce48695f
ab55al <abdullah5590x@gmail.com>
2021-09-27 07:14:10 +0300

    list-configs: ignored files

--- .local/bin/list-configs.sh
+++ .local/bin/list-configs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
-ignore=".dotfiles/.config/zsh/plugins\|\.png$\|LICENSE\|README\|.dotfiles/.git"
+ignore=".dotfiles/.config/zsh/plugins\|\.png$\|LICENSE\|README\|.dotfiles/.git\|awesome/lain\|awesome/freedesktop"
 find "$HOME/.dotfiles" -type f |  grep -v "$ignore" | fzf | xargs -I % nvim %

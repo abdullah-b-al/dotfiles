cd7f1c8442bc69006914a0c70f69d704130ed795
ab55al <abdullah5590x@gmail.com>
2021-10-03 09:18:01 +0300

    Nvim: changed nvim-cmp to accept input only if selected

--- .config/nvim/plugin/nvim-cmp.lua
+++ .config/nvim/plugin/nvim-cmp.lua
@@ -13,7 +13,7 @@ cmp.setup({
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(), -- Close menu and reject selection
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
     },
     sources = {
         { name = 'nvim_lsp' },

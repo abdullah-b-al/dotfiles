66f60905a040fbaeb8dff848e611a4bae0f2f1dc
ab55al <abdullah5590x@gmail.com>
2021-10-25 00:26:01 +0300

    Nvim: misspelling

--- .config/nvim/after/plugin/lua-line.lua
+++ .config/nvim/after/plugin/lua-line.lua
@@ -162,7 +162,7 @@ ins_left {
     end
     for _, client in ipairs(clients) do
       local filetypes = client.config.filetypes
-      if filetypes and im.fn.index(filetypes, buf_ft) ~= -1 then
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
         return client.name
       end
     end

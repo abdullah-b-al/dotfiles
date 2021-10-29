1c9f4d0aa877dee8fcae21d77d69ab6ed39a8348
ab55al <abdullah5590x@gmail.com>
2021-10-29 19:21:37 +0300

    Nvim: clojure lsp proper path

--- .config/nvim/lua/lsps/clojure_server.lua
+++ .config/nvim/lua/lsps/clojure_server.lua
@@ -1,9 +1,10 @@
 local cfg = require("lsps/lsp_signature")
+local path = os.getenv('HOME') .. '/.local/share/nvim/lsp_servers'
 local dir = 'lsps'
 
 require'lspconfig'.clojure_lsp.setup {
     on_attach = require(dir .. '/lsp_on_attach'),
-        cmd = {"/home/ab55al/.local/share/nvim/lsp_servers/clojure_lsp/clojure-lsp"},
+        cmd = {path .. "/clojure_lsp/clojure-lsp"},
     -- nvim-cmp setting
     capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities ()),
     -- LSP signature

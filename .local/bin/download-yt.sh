f5734bd8d991588d221f97aa1ba4cd29fea98aab
ab55al <abdullah5590x@gmail.com>
2022-01-26 10:13:50 +0300

    Bin: download-yt.sh download playlists

--- .local/bin/download-yt.sh
+++ .local/bin/download-yt.sh
@@ -1,23 +1,55 @@
 #!/bin/sh
 url=$(xclip -selection clipboard -o)
-download_path="$HOME/.local/music"
+music_dir="$HOME/.local/music"
 
-notify-send -t 2000 "download-yt.sh" "Running..."
+alias down-yt='yt-dlp --extract-audio --audio-format best "$url"'
+
+single_video_download() {
+    title=$(yt-dlp --get-title --no-playlist "$url")
+    if down-yt -P "$music_dir" --no-playlist; then
+        notify-send -t 5000 "download-yt.sh" "Downloading '$title' has finished successfully."
+    else
+        notify-send -u critical "download-yt.sh" "Downloading '$title' failed."
+    fi
+}
 
 if ! command -v yt-dlp > /dev/null; then
     notify-send -t 5000 -u critical "download-yt.sh" "'yt-dlp' isn't installed."
     exit
 fi
 
-if echo "$url" | grep -qi "youtube.com/watch" && [ ${#url} -le 45 ]; then
-    video_title=$(yt-dlp --get-title "$url")
+if echo "$url" | grep -Ei "youtube.com/channel">/dev/null; then
+    notify-send -t 3000 -u critical "download-yt.sh" "Link of channel. Exiting..."
+    exit
+fi
 
-    if yt-dlp --extract-audio --audio-format flac "$url" -P "$download_path"; then
-        notify-send -t 5000 "download-yt.sh" "Downloading '$video_title' has finished successfully."
-    else
-        notify-send -u critical "download-yt.sh" "Downloading '$video_title' failed."
+notify-send -t 2000 "download-yt.sh" "Running..."
+
+# Playlist link
+if echo "$url" | grep -Ei "(youtube.com/watch|youtu.be).*list">/dev/null; then
+    full="Download full playlist"
+    single="Download single video"
+    choice="$(printf "%s\n%s" "$single" "$full" | dmenu)"
+
+    if [ "$choice" = "$single" ]; then
+        single_video_download
+    elif [ "$choice" = "$full" ]; then
+        dir_name=$(dmenu)
+        # if dir_name is empty give it a random name
+        [ -z "$dir_name" ] && dir_name=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20 ; echo '')
+        mkdir -p "$music_dir/$dir_name"
+        title=$(yt-dlp --get-title "$url")
+
+        if down-yt --yes-playlist -P "$music_dir/$dir_name"; then
+            notify-send -t 5000 "download-yt.sh" "Downloading\n--\n$title\n--\nhas finished successfully."
+        else
+            notify-send -u critical "download-yt.sh" "Downloading playlist failed."
+        fi
     fi
 
+# Single video link
+elif echo "$url" | grep -Ei "(youtube.com/watch|youtu.be)">/dev/null; then
+    single_video_download
 else
-    notify-send -t 3000 -u critical "download-yt.sh" "Input isn't a youtube url or is longer than 45 chars"
+    notify-send -t 3000 -u critical "download-yt.sh" "Input isn't a youtube url"
 fi

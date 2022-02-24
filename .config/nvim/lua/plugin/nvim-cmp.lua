2bab302103d0cc9b2dfdb788a99ca0f1e4adaa5e
ab55al <abdullah5590x@gmail.com>
2022-02-24 19:19:56 +0300

    Nvim: Corrected mapping in cmp

--- .config/nvim/lua/plugin/nvim-cmp.lua
+++ .config/nvim/lua/plugin/nvim-cmp.lua
@@ -12,9 +12,7 @@ cmp.setup({
     mapping = {
         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
         ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(), -- Close menu and reject selection
-        -- ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<C-e>'] = cmp.mapping.abort(),       -- Close menu and reject selection
     },
     sources = {
         { name = 'nvim_lsp' },

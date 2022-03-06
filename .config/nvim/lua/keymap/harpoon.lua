4c59dbb9cfad54e9247a83ea18648948a85294b1
ab55al <abdullah5590x@gmail.com>
2022-03-06 19:26:12 +0300

    Nvim: fmt

--- .config/nvim/lua/keymap/harpoon.lua
+++ .config/nvim/lua/keymap/harpoon.lua
@@ -12,8 +12,8 @@ function _G.harpooning(command, number)
   end
 end
 
-map( 'n', '<leader>gg', ':lua harpooning("open")<CR>', {} , 'Harpoon: Open window')
-map( 'n', '<leader>ga', ':lua harpooning("add_file")<CR>', {}, 'Harpoon: Add file')
-map( 'n', '<leader>gn', ':lua harpooning("nav_file", 1)<CR>', {}, 'Harpoon: Navigate to mark 1')
-map( 'n', '<leader>ge', ':lua harpooning("nav_file", 2)<CR>', {}, 'Harpoon: Navigate to mark 2')
-map( 'n', '<leader>gi', ':lua harpooning("nav_file", 3)<CR>', {}, 'Harpoon: Navigate to mark 3')
+map('n', '<leader>gg', ':lua harpooning("open")<CR>', {} , 'Harpoon: Open window')
+map('n', '<leader>ga', ':lua harpooning("add_file")<CR>', {}, 'Harpoon: Add file')
+map('n', '<leader>gn', ':lua harpooning("nav_file", 1)<CR>', {}, 'Harpoon: Navigate to mark 1')
+map('n', '<leader>ge', ':lua harpooning("nav_file", 2)<CR>', {}, 'Harpoon: Navigate to mark 2')
+map('n', '<leader>gi', ':lua harpooning("nav_file", 3)<CR>', {}, 'Harpoon: Navigate to mark 3')

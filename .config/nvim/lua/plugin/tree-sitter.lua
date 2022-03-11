0ed883f8a956a55309f9e22cb0c90ba0eecd0813
ab55al <abdullah5590x@gmail.com>
2022-03-11 20:37:37 +0300

    Nvim: Treesitter install some languages

--- .config/nvim/lua/plugin/tree-sitter.lua
+++ .config/nvim/lua/plugin/tree-sitter.lua
@@ -1,12 +1,30 @@
 local configs = require("nvim-treesitter.configs")
 
 configs.setup {
-  ensure_installed = "maintained",
+  ensure_installed = {
+    'bash',
+    'c',
+    'clojure',
+    'cmake',
+    'cpp',
+    'css',
+    'html',
+    'json',
+    'json5',
+    'jsonc',
+    'lua',
+    'make',
+    'markdown',
+    'regex',
+    'vim',
+    'zig',
+  },
+
   sync_install = false,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = true,
-
+    additional_vim_regex_highlighting = false,
   },
+
   indent = { enable = true },
 }

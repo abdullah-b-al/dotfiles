0b599990d098f9cf171e2ff2ad9d124d1f31985d
ab55al <abdullah5590x@gmail.com>
2022-03-12 15:19:54 +0300

    Nvim: cmp mappings

--- .config/nvim/lua/plugin/nvim-cmp.lua
+++ .config/nvim/lua/plugin/nvim-cmp.lua
@@ -20,6 +20,8 @@ cmp.setup({
     ['<C-d>'] = cmp.mapping.scroll_docs(4),
     ['<C-e>'] = cmp.mapping.abort(),       -- Close menu and reject selection
     ['<C-c>'] = cmp.mapping.complete(),
+    ['<C-n>'] = cmp.mapping.select_next_item( { behavior = cmp.SelectBehavior.Select }),
+    ['<C-p>'] = cmp.mapping.select_prev_item( { behavior = cmp.SelectBehavior.Select }),
   },
   sources = {
   { name = 'nvim_lsp' },

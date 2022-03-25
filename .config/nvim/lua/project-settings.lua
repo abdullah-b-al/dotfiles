55591df71d0312325f1827f4c0d83342281ca327
ab55al <abdullah5590x@gmail.com>
2022-03-25 21:31:50 +0300

    Nvim: changes

--- .config/nvim/lua/project-settings.lua
+++ .config/nvim/lua/project-settings.lua
@@ -6,27 +6,41 @@ local function m(name)
 end
 
 vim.cmd[[
-  function! Make_build(build_dir)
+  function! CMake_build(build_dir)
   let l:root = luaeval("vim.lsp.buf.list_workspace_folders()[1]")
 
   if l:root == "null"
-    echo "Make_build: Invalid dir"
+    echo "CMake_build: Invalid dir"
     return
   endif
 
   let l:build = l:root . "/" . a:build_dir
-  let l:prev_dir = execute('pwd')
 
   call chdir(l:build)
   make
   call chdir(l:root)
   endfunction
+
+  function! Make_run_build(build_dir)
+  let l:root = luaeval("vim.lsp.buf.list_workspace_folders()[1]")
+
+  if l:root == "null"
+    echo "Make_run_build: Invalid dir"
+    return
+  endif
+
+  let l:build = l:root . "/" . a:build_dir
+
+  call chdir(l:build)
+  split | terminal "$SHELL" -c "make run"
+  call chdir(l:root)
+  endfunction
 ]]
 
 if m('opencv') then
-  vim.opt.makeprg='cmake .. && cmake --build .'
-  map('n', '<F8>', ':wa<CR>:!./run.sh<CR>', {},
+  vim.opt.makeprg='cmake .. && make -i'
+  map('n', '<F8>', ':call Make_run_build("build")<CR>', {},
     'Run code', 0)
-  map('n', '<F3>', ':call Make_build("build")<CR>', {},
+  map('n', '<F3>', ':call CMake_build("build")<CR>', {},
     'Build code', 0)
 end

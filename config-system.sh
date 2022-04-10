9fd353b7be027aa11008b4a9d48aacc188b5802b
ab55al <abdullah5590x@gmail.com>
2022-04-10 21:58:57 +0300

    Install: network

--- config-system.sh
+++ config-system.sh
@@ -36,6 +36,9 @@ chmod -c 0400 /etc/doas.conf
 sed -i "s|# %wheel ALL=(ALL) ALL|%wheel ALL=(ALL) ALL|g" /etc/sudoers
 sed -i "s|# %wheel ALL=(ALL:ALL) ALL|%wheel ALL=(ALL) ALL|g" /etc/sudoers
 
+# Set up network
+systemctl enable NetworkManager
+
 # Config and install grub
 mkdir -p /boot/EFI
 

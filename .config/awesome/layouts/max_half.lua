97ce13e18495f9705ec7c8545bb41c9978a943ad
ab55al <abdullah5590x@gmail.com>
2022-04-21 03:39:37 +0300

    Awesome: Don't half windows if there is only one

--- .config/awesome/layouts/max_half.lua
+++ .config/awesome/layouts/max_half.lua
@@ -3,30 +3,26 @@ local half = {}
 half.name = "half"
 
 function half.arrange(p)
-    local t   = p.tag or screen[p.screen].selected_tag
-    local area = p.workarea
-    local cls = p.clients
+  local t   = p.tag or screen[p.screen].selected_tag
+  local area = p.workarea
+  local cls = p.clients
 
-    local master = true
-    for _, c in pairs(p.clients) do
-        local x
-        if master then
-            x = area.x
-        else
-            x = (area.x + (area.width / 2))
-        end
-        local g = {
-            x = x,
-            y = area.y,
-            width = area.width / 2,
-            height = area.height
-        }
-        p.geometries[c] = g
-        master = false
-    end
+  local master = true
+  for _, c in pairs(p.clients) do
+    local x     = master and area.x or (area.x + (area.width / 2))
+    local width = (#cls == 1) and area.width or (area.width / 2)
+    local g = {
+      x = x,
+      y = area.y,
+      width = width,
+      height = area.height
+    }
+    p.geometries[c] = g
+    master = false
+  end
 end
 function half.skip_gap(nclients, t) -- luacheck: no unused args
-    return true
+  return true
 end
 
 return half

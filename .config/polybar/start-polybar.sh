39b212e1f5f2aeb8dd9aa3665ea6e3d019403612
ab55al <abdullah5590x@gmail.com>
2022-04-25 11:03:36 +0300

    Polybar: a lot of changes

--- .config/polybar/start-polybar.sh
+++ .config/polybar/start-polybar.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-while true;do
-  killall polybar 2>/dev/null
-  if pgrep dwm; then
-    polybar --config=$HOME/.config/polybar/config.ini example
-  fi
+
+DIR="$HOME/.config/polybar"
+
+while true; do
+  killall -q polybar
+
+  # Wait until the processes have been shut down
+  while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+  polybar -q main -c "$DIR"/config.ini
 done

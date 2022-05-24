d6b086312369771cb4d6c96e787a403331211e94
ab55al <abdullah5590x@gmail.com>
2022-05-24 14:45:32 +0300

    ...

--- .local/bin/change-wallpaper.sh
+++ .local/bin/change-wallpaper.sh
@@ -5,11 +5,8 @@ num="$(echo $((RANDOM % num_of_files + 1)))"
 img="$(ls $wallpapers | nl | grep -E "^\s+$num\s" | awk '{print $2}')"
 
 num="$(echo $((RANDOM % num_of_files + 1)))"
-img_2="$(ls $wallpapers | nl | grep -E "^\s+$num\s" | awk '{print $2}')"
-
-num="$(echo $((RANDOM % num_of_files + 1)))"
 lock_img="$(ls $wallpapers | nl | grep -E "^\s+$num\s" | awk '{print $2}')"
 
-feh --no-fehbg --bg-fill $wallpapers/$img $wallpapers/$img_2 &
+feh --no-fehbg --bg-fill $wallpapers/$img &
 wal -i "$wallpapers/$lock_img" -entsq &
 ~/.config/polybar/scripts/pywal.sh $wallpapers/$img

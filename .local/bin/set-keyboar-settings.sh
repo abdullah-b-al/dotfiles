41fe644213678fbf228d4d792236969b24caa421
ab55al <abdullah5590x@gmail.com>
2023-03-05 17:04:44 +0300

    ...

--- .local/bin/set-keyboar-settings.sh
+++ .local/bin/set-keyboar-settings.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-if lsusb | grep -i ergodox; then
-  setxkbmap -layout 'us,ar' -option 'grp:alt_shift_toggle' &
+setxkbmap -layout 'us,ar' -option 'grp:alt_shift_toggle'
+
+if lsusb | grep -i ergodox ; then
+  setxkbmap -layout 'us,ar' -option 'grp:alt_shift_toggle'
 else
-  setxkbmap -layout 'us,ar' -option 'grp:alt_shift_toggle' &
+  xmodmap ~/.dotfiles/colemak_dh.xmodmap
 fi
+
 
 xset r rate 250 40

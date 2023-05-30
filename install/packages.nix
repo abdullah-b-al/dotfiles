233328ffe9c877b8efe0c1c44f48eeb66a23a389
ab55al <abdullah5590x@gmail.com>
2023-05-30 15:50:03 +0300

    ...

--- install/packages.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-[
-  pkgs.brave
-  pkgs.nerdfonts
-  pkgs.i3lock-color
-]
--- install/setup-nix.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-nix-channel --add https://nixos.org/channels/nixpkgs-unstable
-nix-channel --update
-nix-env -if ~/.dotfiles/install/packages.nix

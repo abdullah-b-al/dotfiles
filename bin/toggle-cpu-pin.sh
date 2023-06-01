09bbc319ac8bc7d540edc4020696981f61ad34af
ab55al <abdullah5590x@gmail.com>
2023-06-01 14:35:10 +0300

    ...

--- bin/toggle-cpu-pin.sh
+++ bin/toggle-cpu-pin.sh
@@ -2,14 +2,14 @@
 
 p=$(nproc)
 if [ $p = 12 ]; then
-  sudo -A systemctl set-property --runtime -- user.slice AllowedCPUs=0,1,2,6,7,8
-  sudo -A systemctl set-property --runtime -- system.slice AllowedCPUs=0,1,2,6,7,8
-  sudo -A systemctl set-property --runtime -- init.scope AllowedCPUs=0,1,2,6,7,8
+  sudo systemctl set-property --runtime -- user.slice AllowedCPUs=0,1,2,6,7,8
+  sudo systemctl set-property --runtime -- system.slice AllowedCPUs=0,1,2,6,7,8
+  sudo systemctl set-property --runtime -- init.scope AllowedCPUs=0,1,2,6,7,8
   notify-send -t 3000 CPUs "$(nproc)"
 elif [ $p = 6 ]; then
-  sudo -A systemctl set-property --runtime -- system.slice AllowedCPUs=0-11
-  sudo -A systemctl set-property --runtime -- user.slice AllowedCPUs=0-11
-  sudo -A systemctl set-property --runtime -- init.scope AllowedCPUs=0-11
+  sudo systemctl set-property --runtime -- system.slice AllowedCPUs=0-11
+  sudo systemctl set-property --runtime -- user.slice AllowedCPUs=0-11
+  sudo systemctl set-property --runtime -- init.scope AllowedCPUs=0-11
   notify-send -t 3000 CPUs "$(nproc)"
 fi
 

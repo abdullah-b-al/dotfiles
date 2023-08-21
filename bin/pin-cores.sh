3131eaeb69d116f7ecca78dfad79bd3c7db708db
ab55al <abdullah@abal.xyz>
2023-08-21 12:13:01 +0300

    ...

--- bin/pin-cores.sh
+++ bin/pin-cores.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-zenity --password | sudo -S systemctl set-property --runtime -- user.slice AllowedCPUs=0,1,6,7
+sudo systemctl set-property --runtime -- user.slice AllowedCPUs=0,1,6,7
 sudo systemctl set-property --runtime -- system.slice AllowedCPUs=0,1,6,7
 sudo systemctl set-property --runtime -- init.scope AllowedCPUs=0,1,6,7
 notify-send -t 3000 CPUs "$(nproc)"

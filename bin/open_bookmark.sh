2fe8367aa30617220b24ee21f1e92c4d665b540b
ab55al <abdullah@abal.xyz>
2023-10-17 18:03:53 +0300

    ...

--- bin/open_bookmark.sh
+++ bin/open_bookmark.sh
@@ -2,7 +2,7 @@ set -e
 
 bookmarks="$HOME/personal/bookmarks"
 
-url="$(grep -ve "^http" -ve "^\s*$" $bookmarks | rofi -dmenu | xargs -I {} grep -A 2 {} "$bookmarks" | sed -n "2 p")"
+url="$(grep -ve "^http" -ve "^\s*$" $bookmarks | rofi -dmenu -matching fuzzy -i | xargs -I {} grep -A 2 {} "$bookmarks" | sed -n "2 p")"
 
 [ -z "$url" ] && exit 1
 

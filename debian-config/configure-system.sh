a4e95160088aebb2d954617d033820377bbf51a5
ab55al <abdullah@abal.xyz>
2024-03-17 10:44:44 +0300

    ...

--- debian-config/configure-system.sh
+++ debian-config/configure-system.sh
@@ -7,13 +7,19 @@ dir=$(dirname "$path")
 
 user_name="ab55al"
 
-sed -i "s|bookworm main non-free-firmware|bookworm main non-free non-free-firmware|g" /etc/apt/sources.list
-"$dir"/install_packages
-
 grep "^ar_SA.UTF-8 UTF-8" /etc/locale.gen || {
   printf 'ar_SA.UTF-8 UTF-8\n' >> /etc/locale.gen
   /usr/sbin/locale-gen
 };
+
+hdd_uuid="44f16107-3ca4-4739-b2a3-b4dab98d8cc3"
+if [[ $(file /dev/disk/by-uuid/$hdd_uuid) ]]; then
+  printf "\nUUID=%s /mnt/linuxHDD ext4      	rw,relatime	0 2\n" $hdd_uuid >> /etc/fstab
+  mount -m "/dev/disk/by-uuid/$hdd_uuid" /mnt/linuxHDD
+fi
+
+sed -i "s|bookworm main non-free-firmware|bookworm main non-free non-free-firmware|g" /etc/apt/sources.list
+"$dir"/install_packages
 
 enable_user_sudo=$(printf "%s ALL=(ALL:ALL) ALL" $user_name)
 # sed: insert after pattern
@@ -30,10 +36,5 @@ systemctl enable NetworkManager
 
 sed -i "s|managed=false|managed=true|g" /etc/NetworkManager/NetworkManager.conf
 
-hdd_uuid="44f16107-3ca4-4739-b2a3-b4dab98d8cc3"
-if [[ $(file /dev/disk/by-uuid/$hdd_uuid) ]]; then
-  printf "\nUUID=%s /mnt/linuxHDD ext4      	rw,relatime	0 2\n" $hdd_uuid >> /etc/fstab
-  mount -m "/dev/disk/by-uuid/$hdd_uuid" /mnt/linuxHDD
-fi
 
 echo "system configuration done"

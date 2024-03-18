c3973058847af6d600a69b2fc5d9ae226dab5ef4
ab55al <abdullah@abal.xyz>
2024-03-18 17:26:09 +0300

    ...

--- debian-config/install.sh
+++ debian-config/install.sh
@@ -3,32 +3,11 @@ set -e
 ###########
 # functions
 
-get_password() {
-  [ -z "$user_password" ] && {
-    echo "Enter Password:";
-    read user_password;
-  } || true
-}
-
-# brave {
-#   get_password
-
-#   echo $user_password | sudo -S curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-#   channel="deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"
-#   echo $channel > /tmp/brave-browser-release.list
-#   echo $user_password | sudo -S cp /tmp/brave-browser-release.list /etc/apt/sources.list.d/brave-browser-release.list
-
-#   sudo apt update
-#   sudo apt install --assume-yes brave-browser
-# }
-
 neovim() {
-  get_password
-
   touch $HOME/.config/wgetrc
   wget -O /tmp/nvim.appimage https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
   chmod +x /tmp/nvim.appimage
-  echo $user_password | sudo -S mv /tmp/nvim.appimage /usr/local/bin
+  sudo mv /tmp/nvim.appimage /usr/local/bin
 }
 
 i3lock() {
@@ -40,40 +19,10 @@ i3lock() {
   rm -rf /tmp/i3lock
 }
 
-# libvirt {
-#   get_password
-
-#   echo $user_password | sudo -S apt build-dep --assume-yes libvirt
-
-#   cd /tmp
-
-#   libvirt="libvirt-10.1.0"
-#   tar_libvirt="/tmp/$libvirt.tar.xz"
-#   touch $HOME/.config/wgetrc
-#   [ -f $tar_libvirt ] || wget -O $tar_libvirt https://download.libvirt.org/$libvirt.tar.xz
-#   [ -d $libvirt ] || xz -dc $tar_libvirt | tar xvf -
-
-#   cd $libvirt
-#   meson setup build -Dsystem=true -Ddocs=disabled -Dprefix=/usr/local -Dtests=disabled
-#   ninja -C build
-
-#   echo $user_password | sudo -S ninja -C build install
-#   echo $user_password | sudo -S ldconfig
-
-#   id libvirt-qemu || echo $user_password | sudo -S useradd libvirt-qemu
-#   echo $user_password | sudo -S groupadd -f libvirt-qemu
-#   echo $user_password | sudo -S groupadd -f libvirt
-#   echo $user_password | sudo -S groupadd -f libvirtd
-#   echo $user_password | sudo -S usermod -aG libvirt-qemu libvirt-qemu
-#   echo $user_password | sudo -S usermod -aG libvirt-qemu,libvirtd,libvirt ab55al
-# }
-
 starship() {
-  get_password
-
   curl -sS https://starship.rs/install.sh > /tmp/starship.sh
   chmod +x /tmp/starship.sh
-  echo $user_password | sudo /tmp/starship.sh --yes
+  sudo /tmp/starship.sh --yes
 }
 
 gf2() {

5b1eaaadbdd042b769d38112dd0507bd0bb49e64
ab55al <abdullah@abal.xyz>
2024-03-18 05:39:32 +0300

    ...

--- debian-config/install.sh
+++ debian-config/install.sh
@@ -28,7 +28,7 @@ neovim() {
   touch $HOME/.config/wgetrc
   wget -O /tmp/nvim.appimage https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
   chmod +x /tmp/nvim.appimage
-  echo $user_password | sudo -S mv /tmp/nvim.appimage /usr/bin/nvim
+  echo $user_password | sudo -S mv /tmp/nvim.appimage /usr/local/bin
 }
 
 i3lock() {

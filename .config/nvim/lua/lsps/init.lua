62b7c20066d52c565afb8e1c7b89962969d6e7a0
ab55al <abdullah@abal.xyz>
2024-03-18 15:26:28 +0300

    ...

--- .config/nvim/lua/lsps/init.lua
+++ .config/nvim/lua/lsps/init.lua
@@ -24,6 +24,7 @@ return {
   -- require(dir .. '/lua_server'),
   require(dir .. '/zig_server'),
   require(dir .. '/go_server'),
+  require(dir .. '/ansible'),
   require(dir .. '/python_server'),
   require(dir .. '/cmake_server'),
 }

70edaa568f5e39ac3f2648482d3455efa9fd4ab6
ab55al <abdullah@abal.xyz>
2024-04-26 01:49:11 +0300

    ...

--- bin/tmux-switch-to.sh
+++ bin/tmux-switch-to.sh
@@ -8,13 +8,16 @@ session="$(tmux list-sessions -F "#{session_name},#{session_attached},#{session_
 
 active_window="$(tmux list-windows -t "$session" -F "#{?window_active,#{window_name},}")"
 
-if tmux has-session -t "$session:editor" 2> /dev/null; then
+tmux has-session -t "$session:editor" 2> /dev/null
+has_session="$?"
+
+if [ "$has_session" = 0 ]; then
     if [ "$wanted_window" = "editor" ]; then
         tmux switch-client -t "$session:editor"
     elif [ "$wanted_window" = "shell" ] && [ "$active_window" = "editor" ]; then
         tmux select-window -l -t "$session"
     fi
-else
+elif [ "$has_session" != 0 ] && [ "$wanted_window" = "editor" ]; then
     tmux new-window -d -t "$session:0" -n editor
     tmux switch-client -t "$session:editor"
 fi

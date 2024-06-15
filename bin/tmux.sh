d837d1be61bceaded9bb49c8e7210040e17c1d98
ab55al <abdullah@abal.xyz>
2024-06-16 00:56:16 +0300

    ...

---
+++ bin/tmux.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+sub_and_abs() {
+    if [ $1 -lt $2 ]; then
+        echo "$((($2 - $1)))"
+    else
+        echo "$((($1 - $2)))"
+    fi
+}
+
+active_session() {
+    tmux list-sessions -F "#{session_name},#{session_attached},#{session_activity}" | \
+        sort -r -k3 --field-separator="," | head --lines 1 | cut -d ',' -f 1
+}
+
+nvim_server() {
+    session="$(active_session)"
+    echo "/tmp/nvim-server-$session.pipe"
+}
+
+active_window() {
+    session="$(active_session)"
+    tmux list-windows -t "$session" -F "#{?window_active,#{window_name},}"
+}
+
+has_editor_window() {
+    session="$(active_session)"
+    tmux has-session -t "$session:editor" 2> /dev/null
+    has_editor="$?"
+    [ "$has_editor" = 0 ] && echo yes
+    [ "$has_editor" != 0 ] && echo no
+}
+
+window_count() {
+    session="$(active_session)"
+    tmux display-message -p -t "$session" "#{session_windows}" 2> /dev/null
+}
+
+create_editor_window() {
+    if [ "$(has_editor_window)" = "no" ]; then
+        nvim_server_path="$(nvim_server)"
+        session="$(active_session)"
+        [ -e "$nvim_server_path" ] || tmux new-window -d -t "$session:0" -n editor -d nvim --listen "$nvim_server_path"
+    fi
+}
+
+non_editor_window_index() {
+    # the window index is always the last used non-editor window
+    tmux list-windows -t general -F "#{window_index},#{window_name},#{window_activity}" | \
+        grep -v "editor," | sort -r -k3 --field-separator="," | head --lines 1 | cut -d ',' -f 1
+}
+
+popup_dims() {
+    out="$(tmux display-message -p "#{cursor_x} #{cursor_y} #{pane_top} #{pane_left} #{pane_height} #{pane_width} #{window_height} #{window_width}")"
+    out_x="$(echo "$out" | cut -f 1 -d ' ' )"
+    out_y="$(echo "$out" | cut -f 2 -d ' ' )"
+    out_top="$(echo "$out" | cut -f 3 -d ' ' )"
+    out_left="$(echo "$out" | cut -f 4 -d ' ' )"
+    out_pane_h="$(echo "$out" | cut -f 5 -d ' ' )"
+    out_pane_w="$(echo "$out" | cut -f 6 -d ' ' )"
+    out_win_h="$(echo "$out" | cut -f 7 -d ' ' )"
+    out_win_w="$(echo "$out" | cut -f 8 -d ' ' )"
+
+    w="$out_pane_w"
+    h="25"
+    x="$((($out_x + $out_left)))"
+    x="$(sub_and_abs $x 3)"
+    y="$((($out_y + $out_top + $h + 1)))"
+
+
+    layout="reverse"
+
+    if [ "$y" -gt "$out_win_h" ]; then
+        y="$((($y - $h - 1)))"
+        layout="default"
+    fi
+
+    right="$((($x+$w)))"
+    if [ "$w" = "$out_win_w" ] || [ "$right" -ge "$out_win_w" ]; then
+        w="$(sub_and_abs $x $out_win_w)"
+    fi
+
+    echo "-x $x -y $y -w $w -h $h,$layout"
+}
+
+# If an argument is provided then run the function with the same name
+if ! [ -z "$1" ]; then
+    # Make sure the argument matches one of the functions
+    if ! cat "$0" | grep -q "^$1()\s*{"; then
+        echo "Command doesn't exist"
+        exit 1
+    fi
+
+    "$1"
+fi

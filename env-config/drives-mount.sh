e541e124f3bd330a53b169bb89e6a40d1ace8cb1
abdullah-b-al <abdullah.bander.al@gmail.com>
2025-05-01 15:24:22 +0300

    ...

--- env-config/drives-mount.sh
+++ env-config/drives-mount.sh
@@ -9,7 +9,7 @@ path="/dev/disk/by-uuid/44f16107-3ca4-4739-b2a3-b4dab98d8cc3"
 line="$path $mount_dir ext4 rw,relatime 0 0"
 file="/etc/fstab"
 
-make -p "$mount_dir"
+mkdir -p "$mount_dir"
 
 if [ -e "$path" ]; then
     if ! [ "$(grep -F "$line" "$file")" ]; then
